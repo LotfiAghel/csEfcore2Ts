f8703918720f68308341c1ffa79bfef1b3920dc8
F_L_A <fatholah@gmail.com>
2025-10-06 17:19:17 +0330

    fix

--- RemoveFunctionAnalyzer/ProgramTests.cs
+++ RemoveFunctionAnalyzer/ProgramTests.cs
@@ -9,6 +9,38 @@ namespace RemoveFunctionAnalyzer.Tests
 {
     public class ProgramTests
     {
+         private const string BeforeCode = @"
+class C {
+    void Foo() {}
+    void Bar() {
+        Foo();
+        Foo();
+    }
+}";
+
+        private const string AfterCode = @"
+class C {
+    void Bar() {
+        0;
+        0;
+    }
+}";
+
+        [Fact]
+        public async Task RemoveMethodAndReplaceInvocations_RemovesMethodAndReplacesCalls2()
+        {
+            var tree = CSharpSyntaxTree.ParseText(BeforeCode);
+            var root = await tree.GetRootAsync();
+
+            var methodNode = root.DescendantNodes().OfType<MethodDeclarationSyntax>().First(m => m.Identifier.Text == "Foo");
+
+            var newRoot = RemoveFunctionAnalyzer.Program.RemoveMethodAndReplaceInvocations(root, methodNode, "Foo", "0");
+
+            var newCode = newRoot.ToFullString();
+
+            Assert.Equal(AfterCode.Replace("\r\n", "\n"), newCode.Replace("\r\n", "\n"));
+        }
+
         [Fact]
         public async Task RemoveMethodAndReplaceInvocations_RemovesMethodAndReplacesCalls()
         {
