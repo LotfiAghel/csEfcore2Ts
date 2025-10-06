4d13dee4e5570883e07038e7235e49fccac4a785
F_L_A <fatholah@gmail.com>
2025-10-06 16:25:45 +0330

    first worked vel

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,10 +1,6 @@
-﻿namespace ConsoleApp1
+namespace ConsoleApp1
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
     }
 }
