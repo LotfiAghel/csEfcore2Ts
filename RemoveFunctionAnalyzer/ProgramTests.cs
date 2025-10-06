4df10eb2a0cc9dbbc4c0759c2238b985bc9ae75f
F_L_A <fatholah@gmail.com>
2025-10-06 16:59:11 +0330

    write test

---
+++ RemoveFunctionAnalyzer/ProgramTests.cs
@@ -0,0 +1,91 @@
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Xunit;
+using System.Linq;
+
+namespace RemoveFunctionAnalyzer.Tests
+{
+    public class ProgramTests
+    {
+        [Fact]
+        public async Task RemoveMethodAndReplaceInvocations_RemovesMethodAndReplacesCalls()
+        {
+            var code = @"
+class C {
+    void Foo() {}
+    void Bar() {
+        Foo();
+        Foo();
+    }
+}";
+            var tree = CSharpSyntaxTree.ParseText(code);
+            var root = await tree.GetRootAsync();
+
+            var methodNode = root.DescendantNodes().OfType<MethodDeclarationSyntax>().First(m => m.Identifier.Text == "Foo");
+
+            var newRoot = RemoveFunctionAnalyzer.Program.RemoveMethodAndReplaceInvocations(root, methodNode, "Foo", "0");
+
+            var newCode = newRoot.ToFullString();
+
+            Assert.DoesNotContain("void Foo()", newCode);
+            Assert.DoesNotContain("Foo()", newCode);
+            Assert.Contains("0;", newCode);
+        }
+
+        [Fact]
+        public async Task RemoveClassAndReplaceUsages_ReplacesUsages()
+        {
+            var code = @"
+class C {}
+class D {
+    C c;
+    void M() {
+        C local = null;
+    }
+}";
+            var tree = CSharpSyntaxTree.ParseText(code);
+            var root = await tree.GetRootAsync();
+
+            var classNode = root.DescendantNodes().OfType<ClassDeclarationSyntax>().First(c => c.Identifier.Text == "C");
+
+            var newRoot = RemoveFunctionAnalyzer.Program.RemoveClassAndReplaceUsages(root, classNode, "C", "null");
+
+            var newCode = newRoot.ToFullString();
+
+            Assert.DoesNotContain("class C", newCode);
+            Assert.Contains("null c;", newCode);
+            Assert.Contains("null local = null;", newCode);
+        }
+
+        [Fact]
+        public async Task RemoveClassAndReplaceUsages_WithZero_RemovesDeclarationsAndDerivedClasses()
+        {
+            var code = @"
+class Base {}
+class Derived : Base {}
+class C {}
+class D {
+    C c;
+    Base b;
+    void M() {
+        C local = null;
+    }
+}";
+            var tree = CSharpSyntaxTree.ParseText(code);
+            var root = await tree.GetRootAsync();
+
+            var classNode = root.DescendantNodes().OfType<ClassDeclarationSyntax>().First(c => c.Identifier.Text == "C");
+
+            var newRoot = RemoveFunctionAnalyzer.Program.RemoveClassAndReplaceUsages(root, classNode, "C", "0");
+
+            var newCode = newRoot.ToFullString();
+
+            Assert.DoesNotContain("class C", newCode);
+            Assert.DoesNotContain("C c;", newCode);
+            Assert.DoesNotContain("C local = null;", newCode);
+            Assert.Contains("class Derived : Base", newCode); // Derived class unrelated to C remains
+        }
+    }
+}
